e711a4e11d71e32e1ff309ead2325731e736f2e7
Juan <juancruzm2004@gmail.com>
2024-09-03 17:25:18 -0300

    agrega mapping

--- src/main/java/Usuario/ControladorUsuario.java
+++ src/main/java/Usuario/ControladorUsuario.java
@@ -1,20 +1,30 @@
 package Usuario;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import java.util.Date;
 
 import static Usuario.FactoriaUsuario.CrearUsuarioAdmin;
 import static Usuario.FactoriaUsuario.CrearUsuarioNormal;
 
+
+@RestController
+@RequestMapping("/api/usuario")
 public class ControladorUsuario {
 
+    @GetMapping("/normal")
     public static void registrarUsuario(String nombreUsuario, String mail, String contrasena, String nombre, String apellido, Date fechaNacimiento){
         CrearUsuarioNormal( nombreUsuario,  mail,  contrasena,  nombre,  apellido,  fechaNacimiento);
     }
 
+    @GetMapping("/admin")
     public static void registrarAdministrador(String nombreUsuario, String mail, String contrasena, String nombre, String apellido, Date fechaNacimiento){
         CrearUsuarioAdmin( nombreUsuario,  mail,  contrasena,  nombre,  apellido,  fechaNacimiento);
     }
 
+    @GetMapping("/{id}")
     public static void logearse(String credencial, String contrasena){
 
         if (credencial.contains("@")){
